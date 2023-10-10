e17a7fcd4fc0fca7991baf596966e36e8e68427e
NavaHub <navaa003@gmail.com>
2023-10-10 18:08:30 -0500

    Beginning process of inserting logical code into server.js; added the 'Employee Manager' console log (unecessary but it is provided nonetheless), and in the middle of testing on how user will interact with inquirer prompts.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Import requirements
 const express = require('express');
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const fs = require('fs');
 
@@ -21,3 +22,43 @@ const db = mysql.createConnection(
     console.log('Successfully connected to company_db database.\n')
 );
 
+const questions = [
+    {
+        type: "list",
+        name: "mainMenu",
+        message: "Please select the following options:",
+        choices: ['View all Departments', 'View all Roles', 'View all Employees\n',
+        'View Employees by Manager', 'View Employees by Department\n',
+        'View a Department Budget\n',
+        'Add a Department', 'Add a Role', 'Add an Employee\n',
+        'Update an Employee Role', 'Update Managers\n',
+        'Delete a Department', 'Delete a Role', 'Delete an Employee\n'],
+    },
+    {},
+    {},
+    {},
+    {},
+];
+
+console.log(`
+_______________________________________________________
+|                                                     |
+|    _____                 _                          |
+|   | ____|_ __ ___  _ __ | | ___  _   _  ___  ___    |
+|   |  _| | '_ \` _ \\| '_ \\| |/ _ \\| | | |/ _ \\/ _ \\   |
+|   | |___| | | | | | |_) | | (_) | |_| |  __/  __/   |
+|   |_____|_| |_| |_| .__/|_|\\___/ \\__, |\\___|\\___|   |
+|                   |_|            |___/              |
+|    __  __                                           |
+|   |  \\/  | __ _ _ __   __ _  __ _  ___ _ __         |
+|   | |\\/| |/ _\` | '_ \\ / _\` |/ _\` |/ _ \\ '__|        |
+|   | |  | | (_| | | | | (_| | (_| |  __/ |           |
+|   |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|           |
+|                             |___/                   |
+|                                                     |
+|_____________________________________________________|
+`);
+
+inquirer.prompt(questions).then((data) => {
+    console.log(data);
+})
