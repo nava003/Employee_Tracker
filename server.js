7508b9f83ddea205629168c9fc4dcc6875541fcd
NavaHub <navaa003@gmail.com>
2023-10-11 10:24:28 -0500

    Fleshed out necessary question prompts; welcome title is now read as 'Employee Tracker' vs 'Employee Manager'; noticing there is no loop feature built into inquirer, meaning current approach will require a rehaul later on

--- server.js
+++ server.js
@@ -27,36 +27,48 @@ const questions = [
         type: "list",
         name: "mainMenu",
         message: "Please select the following options:",
-        choices: ['View all Departments', 'View all Roles', 'View all Employees\n',
-        'View Employees by Manager', 'View Employees by Department\n',
-        'View a Department Budget\n',
-        'Add a Department', 'Add a Role', 'Add an Employee\n',
-        'Update an Employee Role', 'Update Managers\n',
-        'Delete a Department', 'Delete a Role', 'Delete an Employee\n'],
+        choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Exit'],
     },
-    {},
-    {},
-    {},
-    {},
+    {
+        type: "list",
+        name: "deptMenu",
+        message: "Select the following Department Options:",
+        choices: ['View a Department Budget', 'Add a Department', 'Delete a Department', 'Go Back'],
+        when: (dAnswer) => dAnswer.mainMenu === 'View all Departments',
+    },
+    {
+        type: "list",
+        name: "roleMenu",
+        message: "Select the following Role Options:",
+        choices: ['Add a Role', 'Delete a Role', 'Go Back'],
+        when: (rAnswer) => rAnswer.mainMenu === 'View all Roles',
+    },
+    {
+        type: "list",
+        name: "empMenu",
+        message: "Select the following Employee Options:",
+        choices: ['View Employees by Manager', 'View Employees by Department',
+        'Update an Employee Role', 'Update Managers',
+        'Add an Employee', 'Delete an Employee', '\nGo Back\n'],
+        when: (eAnswer) => eAnswer.mainMenu === 'View all Employees',
+    },
 ];
 
 console.log(`
-_______________________________________________________
-|                                                     |
-|    _____                 _                          |
-|   | ____|_ __ ___  _ __ | | ___  _   _  ___  ___    |
-|   |  _| | '_ \` _ \\| '_ \\| |/ _ \\| | | |/ _ \\/ _ \\   |
-|   | |___| | | | | | |_) | | (_) | |_| |  __/  __/   |
-|   |_____|_| |_| |_| .__/|_|\\___/ \\__, |\\___|\\___|   |
-|                   |_|            |___/              |
-|    __  __                                           |
-|   |  \\/  | __ _ _ __   __ _  __ _  ___ _ __         |
-|   | |\\/| |/ _\` | '_ \\ / _\` |/ _\` |/ _ \\ '__|        |
-|   | |  | | (_| | | | | (_| | (_| |  __/ |           |
-|   |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|           |
-|                             |___/                   |
-|                                                     |
-|_____________________________________________________|
+____________________________________________________________
+|                                                          |
+|   _____                    _                             |
+|  | ____| _ __ ___   _ __  | |  ___   _   _   ___   ___   |
+|  |  _|  | '_ \` _ \\ | '_ \\ | | / _ \\ | | | | / _ \\ / _ \\  |
+|  | |___ | | | | | || |_) || || (_) || |_| ||  __/|  __/  |
+|  |_____||_| |_| |_|| .__/ |_| \\___/  \\__, | \\___| \\___|  |
+|   _____            |_|    _          |___/               |
+|  |_   _|_ __  __ _   ___ | | __ ___  _ __                |
+|    | | | '__|/ _\` | / __|| |/ // _ \\| '__|               |
+|    | | | |  | (_| || (__ |   <|  __/| |                  |
+|    |_| |_|   \\__,_| \\___||_|\\_\\\\___||_|                  |
+|                                                          |
+|__________________________________________________________|
 `);
 
 inquirer.prompt(questions).then((data) => {
