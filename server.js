e9165c29dabe673f94462afeab8c3a1e2741feb8
NavaHub <navaa003@gmail.com>
2023-10-15 23:17:08 -0500

    Complete rehaul of inquirer questions; able to use Separator to avoid using multiple inquirer when conditions; created compDB to encompass schema and seeds contents, and run them via db.query() to avoid performing separate sql actions per node run of server.js; included a ascii-text message when user is done interacting with the application

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const db = mysql.createConnection(
         host: 'localhost' || '127.0.0.1',
         user: 'root',
         password: '`5YDX#OO_&2r/L6}Nz',
-        database: 'company_db'
+        database: 'company_db',
+        multipleStatements: true
     },
     console.log('Successfully connected to company_db database.\n')
 );
@@ -27,50 +28,104 @@ const questions = [
         type: "list",
         name: "mainMenu",
         message: "Please select the following options:",
-        choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Exit'],
+        choices: [
+        'View all Departments',
+        'View a Department Budget',
+        'Add a Department',
+        'Delete a Department',
+        new inquirer.Separator(),
+        'View all Roles',
+        'Add a Role',
+        'Delete a Role',
+        new inquirer.Separator(),
+        'View all Employees',
+        'View Employees by Manager',
+        'View Employees by Department',
+        'Update an Employee Role',
+        'Update Managers',
+        'Add an Employee',
+        'Delete an Employee',
+        new inquirer.Separator(),
+        'Exit',
+        new inquirer.Separator(),
+    ],
     },
     {
         type: "list",
         name: "deptMenu",
         message: "Select the following Department Options:",
-        choices: ['View a Department Budget', 'Add a Department', 'Delete a Department', 'Go Back'],
+        choices: ['Go Back'],
         when: (dAnswer) => dAnswer.mainMenu === 'View all Departments',
     },
     {
         type: "list",
         name: "roleMenu",
         message: "Select the following Role Options:",
-        choices: ['Add a Role', 'Delete a Role', 'Go Back'],
+        choices: ['Go Back'],
         when: (rAnswer) => rAnswer.mainMenu === 'View all Roles',
     },
     {
         type: "list",
         name: "empMenu",
         message: "Select the following Employee Options:",
-        choices: ['View Employees by Manager', 'View Employees by Department',
-        'Update an Employee Role', 'Update Managers',
-        'Add an Employee', 'Delete an Employee', '\nGo Back\n'],
+        choices: ['Go Back'],
         when: (eAnswer) => eAnswer.mainMenu === 'View all Employees',
     },
+    {
+        type: "confirm",
+        name: "confirmExit",
+        message: "Do you wish to exit?",
+        when: (ext) => ext.mainMenu === 'Exit',
+    }
 ];
 
-console.log(`
-____________________________________________________________
-|                                                          |
-|   _____                    _                             |
-|  | ____| _ __ ___   _ __  | |  ___   _   _   ___   ___   |
-|  |  _|  | '_ \` _ \\ | '_ \\ | | / _ \\ | | | | / _ \\ / _ \\  |
-|  | |___ | | | | | || |_) || || (_) || |_| ||  __/|  __/  |
-|  |_____||_| |_| |_|| .__/ |_| \\___/  \\__, | \\___| \\___|  |
-|   _____            |_|    _          |___/               |
-|  |_   _|_ __  __ _   ___ | | __ ___  _ __                |
-|    | | | '__|/ _\` | / __|| |/ // _ \\| '__|               |
-|    | | | |  | (_| || (__ |   <|  __/| |                  |
-|    |_| |_|   \\__,_| \\___||_|\\_\\\\___||_|                  |
-|                                                          |
-|__________________________________________________________|
-`);
+function init() {
+    let compDB = fs.readFileSync('./db/schema.sql', 'utf8', (err) => err ? console.error(err) : console.log("Schema successfully read."));
+    db.query(`${compDB}`, (err) => err ? console.error(err) : console.log("Schema successfully executed."));
 
-inquirer.prompt(questions).then((data) => {
-    console.log(data);
-})
+    compDB = fs.readFileSync('./db/seeds.sql', 'utf8', (err) => err ? console.log(err) : console.log("Seeds successfully read."));
+    db.query(`${compDB}`, (err) => err ? console.error(err) : console.log("Seeds successfully executed."));
+    
+    console.log(`
+    ____________________________________________________________
+    |                                                          |
+    |   _____                    _                             |
+    |  | ____| _ __ ___   _ __  | |  ___   _   _   ___   ___   |
+    |  |  _|  | '_ \` _ \\ | '_ \\ | | / _ \\ | | | | / _ \\ / _ \\  |
+    |  | |___ | | | | | || |_) || || (_) || |_| ||  __/|  __/  |
+    |  |_____||_| |_| |_|| .__/ |_| \\___/  \\__, | \\___| \\___|  |
+    |   _____            |_|    _          |___/               |
+    |  |_   _|_ __  __ _   ___ | | __ ___  _ __                |
+    |    | | | '__|/ _\` | / __|| |/ // _ \\| '__|               |
+    |    | | | |  | (_| || (__ |   <|  __/| |                  |
+    |    |_| |_|   \\__,_| \\___||_|\\_\\\\___||_|                  |
+    |                                                          |
+    |__________________________________________________________|
+    `);
+
+    inquirer.prompt(questions)
+        .then((data) => {
+            if (!data.confirmExit) {
+                init();
+            }
+
+            
+        }
+    );
+
+    console.log(`
+    ____________________________________________________________
+    |                                                          |
+    |     ____                    _  _                   _     |
+    |    / ___|  ___    ___    __| || |__   _   _   ___ | |    |
+    |   | |  _  / _ \  / _ \  / _\` || '_ \\ | | | | / _ \\| |    |
+    |   | |_| || (_) || (_) || (_| || |_) || |_| ||  __/|_|    |
+    |    \\____| \\___/  \\___/  \\__,_||_.__/  \\__, | \\___|(_)    |
+    |                                       |___/              |
+    |                                                          |
+    |__________________________________________________________|
+    `);
+
+}
+
+init();
