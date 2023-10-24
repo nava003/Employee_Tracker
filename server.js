a71cf4a05beef08f2bddd38f9196fc5b6b7cf7be
NavaHub <navaa003@gmail.com>
2023-10-24 00:53:23 -0500

    Another rehaul of inquirer logic; with the assistance of a TA, there will be one initial inquirer running the 'main menu' of the application, and the user choice will be assigned to a const variable; init function will run necessary setup queries and a welcome console log message; runInquirer function is async and will, for now, carry all of the user choices and inner inquirer prompts and sql queries; after 'main menu' prompt, a Switch-Case method will further break down into either an sql query result, or a further sub-inquirer prompt, depending on the choice the user initially made in 'main menu'; if the user has chosen the 'Exit' choice, a message will be displayed before exiting the terminal application; so far, viewing all departments and viewing a department's budget has been fully fleshed out, with the other Cases left with an initial logic setup.

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 // Import requirements
-const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const fs = require('fs');
 
-// Initialize port and instance for Express.js
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Middleware to parse application/json and urlencoded data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+// Opening a connection to a mysql database named company_db.
 const db = mysql.createConnection(
     {
         host: 'localhost' || '127.0.0.1',
@@ -23,109 +15,203 @@ const db = mysql.createConnection(
     console.log('Successfully connected to company_db database.\n')
 );
 
-const questions = [
+const dbPromise = db.promise();
+
+const uestions = [
     {
-        type: "list",
-        name: "mainMenu",
-        message: "Please select the following options:",
-        choices: [
-        'View all Departments',
-        'View a Department Budget',
-        'Add a Department',
-        'Delete a Department',
-        new inquirer.Separator(),
-        'View all Roles',
-        'Add a Role',
-        'Delete a Role',
-        new inquirer.Separator(),
-        'View all Employees',
-        'View Employees by Manager',
-        'View Employees by Department',
-        'Update an Employee Role',
-        'Update Managers',
-        'Add an Employee',
-        'Delete an Employee',
-        new inquirer.Separator(),
-        'Exit',
-        new inquirer.Separator(),
-    ],
+        type: "input",
+        name: "newDept",
+        message: "Insert new Department name:",
+        when: (dAns) => dAns.mainMenu === 'Add a Department',
     },
     {
-        type: "list",
-        name: "deptMenu",
-        message: "Select the following Department Options:",
-        choices: ['Go Back'],
-        when: (dAnswer) => dAnswer.mainMenu === 'View all Departments',
+        type: "input",
+        name: "delDept",
+        message: "Insert the name of the Department to delete:",
+        when: (rAnswer) => rAnswer.mainMenu === 'Delete a Department',
     },
     {
-        type: "list",
-        name: "roleMenu",
-        message: "Select the following Role Options:",
-        choices: ['Go Back'],
-        when: (rAnswer) => rAnswer.mainMenu === 'View all Roles',
-    },
-    {
-        type: "list",
-        name: "empMenu",
+        type: "input",
+        name: "addEmp",
         message: "Select the following Employee Options:",
         choices: ['Go Back'],
         when: (eAnswer) => eAnswer.mainMenu === 'View all Employees',
     },
-    {
-        type: "confirm",
-        name: "confirmExit",
-        message: "Do you wish to exit?",
-        when: (ext) => ext.mainMenu === 'Exit',
-    }
 ];
 
 function init() {
-    let compDB = fs.readFileSync('./db/schema.sql', 'utf8', (err) => err ? console.error(err) : console.log("Schema successfully read."));
-    db.query(`${compDB}`, (err) => err ? console.error(err) : console.log("Schema successfully executed."));
+    let compDB = fs.readFileSync('./db/schema.sql', 'utf8');
+    db.query(`${compDB}`);
 
-    compDB = fs.readFileSync('./db/seeds.sql', 'utf8', (err) => err ? console.log(err) : console.log("Seeds successfully read."));
-    db.query(`${compDB}`, (err) => err ? console.error(err) : console.log("Seeds successfully executed."));
+    compDB = fs.readFileSync('./db/seeds.sql', 'utf8');
+    db.query(`${compDB}`);
     
     console.log(`
-    ____________________________________________________________
-    |                                                          |
-    |   _____                    _                             |
-    |  | ____| _ __ ___   _ __  | |  ___   _   _   ___   ___   |
-    |  |  _|  | '_ \` _ \\ | '_ \\ | | / _ \\ | | | | / _ \\ / _ \\  |
-    |  | |___ | | | | | || |_) || || (_) || |_| ||  __/|  __/  |
-    |  |_____||_| |_| |_|| .__/ |_| \\___/  \\__, | \\___| \\___|  |
-    |   _____            |_|    _          |___/               |
-    |  |_   _|_ __  __ _   ___ | | __ ___  _ __                |
-    |    | | | '__|/ _\` | / __|| |/ // _ \\| '__|               |
-    |    | | | |  | (_| || (__ |   <|  __/| |                  |
-    |    |_| |_|   \\__,_| \\___||_|\\_\\\\___||_|                  |
-    |                                                          |
-    |__________________________________________________________|
+____________________________________________________________
+|                                                          |
+|   _____                    _                             |
+|  | ____| _ __ ___   _ __  | |  ___   _   _   ___   ___   |
+|  |  _|  | '_ \` _ \\ | '_ \\ | | / _ \\ | | | | / _ \\ / _ \\  |
+|  | |___ | | | | | || |_) || || (_) || |_| ||  __/|  __/  |
+|  |_____||_| |_| |_|| .__/ |_| \\___/  \\__, | \\___| \\___|  |
+|   _____            |_|    _          |___/               |
+|  |_   _|_ __  __ _   ___ | | __ ___  _ __                |
+|    | | | '__|/ _\` | / __|| |/ // _ \\| '__|               |
+|    | | | |  | (_| || (__ |   <|  __/| |                  |
+|    |_| |_|   \\__,_| \\___||_|\\_\\\\___||_|                  |
+|                                                          |
+|__________________________________________________________|
     `);
 
-    inquirer.prompt(questions)
-        .then((data) => {
-            if (!data.confirmExit) {
-                init();
-            }
+    runInquirer();
+};
 
-            
+async function runInquirer() {
+    const data = await inquirer.prompt(
+        {
+            type: "list",
+            name: "mainMenu",
+            message: "Please select the following options:",
+            choices: [
+                'View all Departments',
+                'View a Department Budget',
+                'Add a Department',
+                'Delete a Department',
+                new inquirer.Separator(),
+                'View all Roles',
+                'Add a Role',
+                'Delete a Role',
+                new inquirer.Separator(),
+                'View all Employees',
+                'View Employees by Manager',
+                'View Employees by Department',
+                'Add an Employee',
+                'Update an Employee Role',
+                'Update Managers',
+                'Delete an Employee',
+                new inquirer.Separator(),
+                'Exit',
+                new inquirer.Separator(),
+            ],
         }
-    );
+    )
+    
+    switch (data.mainMenu) {
+        case 'View all Departments':
+            db.query('SELECT * FROM departments', (err, results) => {
+                // const dbResult = results.map();
+                console.table(results);
+                // console.table(dbResult);
+                runInquirer();
+            });
+            break;
+        case 'View a Department Budget':
+            let [rows, fields] = await dbPromise.query('SELECT name FROM departments');
+            // console.log(rows);
+            const deptNames = rows.map(res => res.name);
+            // console.log(mapNames);
+            const deptData = await inquirer.prompt(
+                {
+                    type: 'list',
+                    name: 'deptList',
+                    message: 'Select a Department to view its budget:',
+                    choices: deptNames,
+                }
+            )
 
-    console.log(`
-    ____________________________________________________________
-    |                                                          |
-    |     ____                    _  _                   _     |
-    |    / ___|  ___    ___    __| || |__   _   _   ___ | |    |
-    |   | |  _  / _ \  / _ \  / _\` || '_ \\ | | | | / _ \\| |    |
-    |   | |_| || (_) || (_) || (_| || |_) || |_| ||  __/|_|    |
-    |    \\____| \\___/  \\___/  \\__,_||_.__/  \\__, | \\___|(_)    |
-    |                                       |___/              |
-    |                                                          |
-    |__________________________________________________________|
-    `);
+            // console.log(deptData.deptList);
+            
+            db.query(`SELECT departments.name, SUM(roles.salary) AS dept_budget
+            FROM departments, employees, roles
+            WHERE name = ?
+                AND departments.id = roles.department_id
+                AND roles.id = employees.role_id
+            GROUP BY name`,
+            deptData.deptList, (err, results) => {
+                console.table(results);
+                console.log('\n');
+                runInquirer();
+            });
+            break;
+        case 'View all Roles':
+            db.query('SELECT * FROM roles', (err, results) => {
+                console.log(results);
+            });
+            break;
+        case 'View all Employees':
+            db.query('SELECT * FROM employees', (err, results) => {
+                console.log(results);
+            });
+            break;
+        case 'View Employees by Manager':
+            db.query('', (err, results) => {
 
-}
+            })
+            break;
+        case 'View Employees by Department':
+            db.query('', (err, results) => {
+                
+            })
+            break;
+        case 'Add a Department':
+            db.query('', (err, results) => {
+                
+            })
+            break;
+        case 'Add a Role':
+            db.query('', (err, results) => {
+                
+            })
+            break;
+        case 'Add an Employee':
+            db.query('', (err, results) => {
+                
+            })
+            break;
+        case 'Update an Employee Role':
+            db.query('', (err, results) => {
+                
+            })
+            break;
+        case 'Update Managers':
+            db.query('', (err, results) => {
+                
+            })
+            break;
+        case 'Delete a Department':
+            db.query('', (err, results) => {
+                
+            })
+            break;
+        case 'Delete a Role':
+            db.query('', (err, results) => {
+                
+            })
+            break;
+        case 'Delete an Employee':
+            db.query('', (err, results) => {
+                
+            })
+            break;
+        default:
+            break;
+    }
+
+    if (data.mainMenu === 'Exit') {
+        console.log(`
+____________________________________________________________
+|                                                          |
+|     ____                    _  _                   _     |
+|    / ___|  ___    ___    __| || |__   _   _   ___ | |    |
+|   | |  _  / _ \\  / _ \\  / _\` || '_ \\ | | | | / _ \\| |    |
+|   | |_| || (_) || (_) || (_| || |_) || |_| ||  __/|_|    |
+|    \\____| \\___/  \\___/  \\__,_||_.__/  \\__, | \\___|(_)    |
+|                                       |___/              |
+|                                                          |
+|__________________________________________________________|
+        `);
+        process.exit();
+    }
+};
 
 init();
