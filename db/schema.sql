468439ed8316a294b8d0161d4f1eecea899ef4c0
NavaHub <navaa003@gmail.com>
2023-10-26 18:10:05 -0500

    Slight fix in table setup; update set null is applied to roles and employees tables; foreign keys are properly applied

--- db/schema.sql
+++ db/schema.sql
@@ -15,6 +15,7 @@ CREATE TABLE roles (
     department_id INT,
     FOREIGN KEY (department_id)
         REFERENCES departments(id)
+        ON UPDATE SET NULL
         ON DELETE SET NULL
 );
 
@@ -23,5 +24,13 @@ CREATE TABLE employees (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    FOREIGN KEY (role_id)
+        REFERENCES roles(id)
+        ON UPDATE SET NULL
+        ON DELETE SET NULL,
+  	FOREIGN KEY (manager_id)
+  		REFERENCES employees(id)
+        ON UPDATE SET NULL
+  		ON DELETE SET NULL
 );
